a4cfd74f4d1664c44b6533b1954b6bb79be2a333
OggiDanailov <ogidan@abv.bg>
2019-02-19 23:17:44 -0500

    half way through

--- index.js
+++ index.js
@@ -1,18 +1,128 @@
-var countryEntry = document.getElementById("country-entry")	
-var submit = document.getElementById("submit")
 
-submit.addEventListener('click', function(){
-	ajaxCall()
+// function ajaxCall(){
+// 	$.ajax({url: "https://raw.githubusercontent.com/OggiDanailov/gdp-data/master/data.json",
+// 		type: "GET",
+// 	    dataType: 'json',
+// 		success: function(res){
+// 			ajaxCall2("Bulgaria")
+// 		}
+// 	})
+// }
+
+
+// function createDropDown(res){
+// 	let select = document.createElement("select")
+// 	document.body.appendChild(select)
+// 	for(let i = 0;i<res.length;i++){
+// 		let option = document.createElement("option")
+// 		option.value = res[i]["Country Name"]
+// 		option.innerHTML = res[i]["Country Name"]
+// 		select.appendChild(option)
+// 		ajaxCall2(option.value)
+// 	}
+
+// }
+
+const countryList = document.getElementById("country-list")
+var currentOption = "";
+// function that selects country from the dropdown menu
+countryList.addEventListener('change', function(){
+	ajaxCallCountries().then(function(data){
+		ajaxCallGDP()
+	})
 })
-
-function ajaxCall(){
-	$.ajax({url: "http://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?format=json",
+	
+// ajax call that gets the info about the selected country
+function ajaxCallCountries(){
+		currentOption = countryList.options[countryList.selectedIndex].innerHTML
+	return new Promise((resolve, reject) => {
+		$.ajax({url: "https://restcountries.eu/rest/v2/name/" + currentOption + "?fullText=true",
 		type: "GET",
 	    dataType: 'json',
-		success: function(res){
-				console.log(res)
+			success: function(res){
+				resolve(createFields(res[0]))
 			}
+		})
 	})
 }
+
+
+// function that creates and populates the fields of name, area, population capital and flag
+function createFields(country){
+	// console.log(country)
+	let container = document.createElement('div')
+		container.style.width = '100%'
+		container.style.margin = "20px auto";
+		container.style.border = '2px solid orange'
+		container.style.display = "grid"
+		container.style.gridTemplateColumns = '1fr 1fr'
+		document.body.appendChild(container)
+		let boxLeft = document.createElement("div")
+		boxLeft.setAttribute("id", "boxLeft")
+		// boxLeft.style.background = 'green'
+		boxLeft.style.display = "grid"
+		boxLeft.style.gridTemplateColumns = '1fr 1fr 1fr 1fr'
+		boxLeft.style.border = '1px solid yellow'
+		container.appendChild(boxLeft)
+		let countryName = document.createElement("div")
+		countryName.innerHTML = "Name: " +  country.name
+		boxLeft.appendChild(countryName)
+		let countryArea = document.createElement("div")
+		countryArea.innerHTML ="Area: " + country.area + "km2"
+		boxLeft.appendChild(countryArea)
+		let countryPopulation = document.createElement("div")
+		countryPopulation.innerHTML = "Population: " +  country.population
+		boxLeft.appendChild(countryPopulation)
+		let countryCapital = document.createElement("div")
+		countryCapital.innerHTML = "Capital: " +  country.capital
+		boxLeft.appendChild(countryCapital)
+
+		let countryFlag = document.createElement('div')
+		countryFlag.style.gridColumnStart = "2"
+		countryFlag.style.gridColumnEnd = "4"
+		countryFlag.style.height = "100px"
+		countryFlag.style.backgroundImage = "url(" +  country.flag + ")"
+		countryFlag.style.backgroundSize = "100% 100%"
+		countryFlag.style.border = "1px solid"
+		boxLeft.appendChild(countryFlag)
+}
+
+// second ajax call function that brings the info about GDP 
+function ajaxCallGDP(){
+		$.ajax({url: "https://raw.githubusercontent.com/OggiDanailov/gdp-data/master/data.json",
+		type: "GET",
+	    dataType: 'json',
+			success: function(res){
+				getGDP(res)
+			}
+		})
+}
+	
+function getGDP(data){
+	boxLeft = document.getElementById("boxLeft")
+	let countryGDP = document.createElement("div")
+	countryGDP.style.gridColumnStart = '1'
+	countryGDP.style.gridColumnEnd = "3"
+	countryGDP.style.gridRowStart = "3"
+	countryGDP.style.gridRowEnd = "4"
+	for(let i = 0;i<data.length;i++){
+		if(currentOption == data[i]["Country Name"]){
+			console.log(data[i]["2018"])
+			if(data[i]["2018"] == ""){
+				countryGDP.innerHTML = "GDP for 2017: " +  numberWithCommas(data[i]["2017"])
+				boxLeft.appendChild(countryGDP)	
+			}else {
+				countryGDP.innerHTML = "GDP for 2018: " +  numberWithCommas(data[i]["2018"])
+				boxLeft.appendChild(countryGDP)	
+			}
+		} 
+	}
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+
 
 

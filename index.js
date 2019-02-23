d9b9c264657523f435ce5438ad923906ba7d62db
OggiDanailov <ogidan@abv.bg>
2019-02-22 23:06:48 -0500

    comparing button works and does compare the dtes of the two entries

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 
 
 const countryList = document.getElementById("country-list")
+const countryListRight = document.getElementById("country-list-right")
+const compare = document.getElementById("compare")
 var currentOption = "";
+var dataArrayLeft = [];
+var dataArrayRight = [];
+
 // function that selects country from the dropdown menu
 countryList.addEventListener('change', function(){
 	ajaxCallCountries().then(function(data){
 		ajaxCallGDP()
+	})
+})
+
+countryListRight.addEventListener('change', function(){
+	ajaxCallCountriesRight().then(function(data){
+		ajaxCallGDPRight()
 	})
 })
 	
@@ -17,7 +28,20 @@ function ajaxCallCountries(){
 		type: "GET",
 	    dataType: 'json',
 			success: function(res){
-				resolve(createFields(res[0]))
+				resolve(createFieldsLeft(res[0]))
+			}
+		})
+	})
+}
+
+function ajaxCallCountriesRight(){
+		currentOption = countryListRight.options[countryListRight.selectedIndex].innerHTML
+	return new Promise((resolve, reject) => {
+		$.ajax({url: "https://restcountries.eu/rest/v2/name/" + currentOption + "?fullText=true",
+		type: "GET",
+	    dataType: 'json',
+			success: function(res){
+				resolve(createFieldsRight(res[0]))
 			}
 		})
 	})
@@ -25,8 +49,8 @@ function ajaxCallCountries(){
 
 
 // function that creates and populates the fields of name, area, population capital and flag
-function createFields(country){
-	// console.log(country)
+function createFieldsLeft(country){
+	console.log(country)
 	let container = document.createElement('div')
 		container.style.width = '100%'
 		container.style.margin = "20px auto";
@@ -40,16 +64,19 @@ function createFields(country){
 		// boxLeft.style.background = 'green'
 		boxLeft.style.display = "grid"
 		boxLeft.style.gridTemplateColumns = '1fr 1fr 1fr 1fr'
+		boxLeft.style.gridGap = '5px'
 		boxLeft.style.border = '1px solid yellow'
 		container.appendChild(boxLeft)
 		let countryName = document.createElement("div")
 		countryName.innerHTML = "Name: " +  country.name
+		dataArrayLeft.push(country.name)
 		boxLeft.appendChild(countryName)
 		let countryArea = document.createElement("div")
 		countryArea.innerHTML ="Area: " + numberWithCommas(country.area) + "km2"
 		boxLeft.appendChild(countryArea)
 		let countryPopulation = document.createElement("div")
 		countryPopulation.innerHTML = "Population: " +  numberWithCommas(country.population)
+		dataArrayLeft.push(country.population)
 		boxLeft.appendChild(countryPopulation)
 		let countryCapital = document.createElement("div")
 		countryCapital.innerHTML = "Capital: " +  country.capital
@@ -65,6 +92,49 @@ function createFields(country){
 		boxLeft.appendChild(countryFlag)
 }
 
+function createFieldsRight(country){
+	console.log(country)
+	let container = document.createElement('div')
+		container.style.width = '100%'
+		container.style.margin = "20px auto";
+		container.style.border = '2px solid orange'
+		container.style.display = "grid"
+		container.style.gridTemplateColumns = '1fr 1fr'
+		container.setAttribute("id", "container")
+		document.body.appendChild(container)
+		let boxRight = document.createElement("div")
+		boxRight.setAttribute("id", "boxRight")
+		// boxLeft.style.background = 'green'
+		boxRight.style.display = "grid"
+		boxRight.style.gridTemplateColumns = '1fr 1fr 1fr 1fr'
+		boxRight.style.gridGap = '5px'
+		boxRight.style.border = '1px solid yellow'
+		// container = document.getElementById("container")
+		container.appendChild(boxRight)
+		let countryName = document.createElement("div")
+		countryName.innerHTML = "Name: " +  country.name
+		dataArrayRight.push(country.name)
+		boxRight.appendChild(countryName)
+		let countryArea = document.createElement("div")
+		countryArea.innerHTML ="Area: " + numberWithCommas(country.area) + "km2"
+		boxRight.appendChild(countryArea)
+		let countryPopulation = document.createElement("div")
+		countryPopulation.innerHTML = "Population: " +  numberWithCommas(country.population)
+		dataArrayRight.push(country.population)
+		boxRight.appendChild(countryPopulation)
+		let countryCapital = document.createElement("div")
+		countryCapital.innerHTML = "Capital: " +  country.capital
+		boxRight.appendChild(countryCapital)
+
+		let countryFlag = document.createElement('div')
+		countryFlag.style.gridColumnStart = "2"
+		countryFlag.style.gridColumnEnd = "4"
+		countryFlag.style.height = "100px"
+		countryFlag.style.backgroundImage = "url(" +  country.flag + ")"
+		countryFlag.style.backgroundSize = "100% 100%"
+		countryFlag.style.border = "1px solid"
+		boxRight.appendChild(countryFlag)
+}
 // second ajax call function that brings the info about GDP 
 function ajaxCallGDP(){
 		$.ajax({url: "https://raw.githubusercontent.com/OggiDanailov/gdp-data/master/data.json",
@@ -75,7 +145,18 @@ function ajaxCallGDP(){
 			}
 		})
 }
-	
+
+function ajaxCallGDPRight(){
+		$.ajax({url: "https://raw.githubusercontent.com/OggiDanailov/gdp-data/master/data.json",
+		type: "GET",
+	    dataType: 'json',
+			success: function(res){
+				getGDPRight(res)
+			}
+		})
+}
+
+
 function getGDP(data){
 	boxLeft = document.getElementById("boxLeft")
 	let countryGDP = document.createElement("div")
@@ -88,10 +169,35 @@ function getGDP(data){
 			console.log(data[i]["2018"])
 			if(data[i]["2018"] == ""){
 				countryGDP.innerHTML = "GDP for 2017: " +  numberWithCommas(data[i]["2017"])
+				dataArrayLeft.push(data[i]["2017"])
 				boxLeft.appendChild(countryGDP)	
 			}else {
 				countryGDP.innerHTML = "GDP for 2018: " +  numberWithCommas(data[i]["2018"])
+				dataArrayLeft.push(data[i]["2018"])
 				boxLeft.appendChild(countryGDP)	
+			}
+		} 
+	}
+}
+
+function getGDPRight(data){
+	boxRight = document.getElementById("boxRight")
+	let countryGDP = document.createElement("div")
+	countryGDP.style.gridColumnStart = '1'
+	countryGDP.style.gridColumnEnd = "3"
+	countryGDP.style.gridRowStart = "3"
+	countryGDP.style.gridRowEnd = "4"
+	for(let i = 0;i<data.length;i++){
+		if(currentOption == data[i]["Country Name"]){
+			console.log(data[i]["2018"])
+			if(data[i]["2018"] == ""){
+				countryGDP.innerHTML = "GDP for 2017: " +  numberWithCommas(data[i]["2017"])
+				dataArrayRight.push(data[i]["2017"])
+				boxRight.appendChild(countryGDP)	
+			}else {
+				countryGDP.innerHTML = "GDP for 2018: " +  numberWithCommas(data[i]["2018"])
+				dataArrayRight.push(data[i]["2018"])
+				boxRight.appendChild(countryGDP)	
 			}
 		} 
 	}
@@ -99,6 +205,16 @@ function getGDP(data){
 
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+
+compare.addEventListener("click", doComparison)
+
+function doComparison(){
+	console.log("The gdp per person in " +  dataArrayLeft[0] + " is " + dataArrayLeft[2]/dataArrayLeft[1] +"$ per year")
+
+	
+	
 }
 
 
